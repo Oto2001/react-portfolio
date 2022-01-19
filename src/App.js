3d90e25b549b5bca930ebd1a224eceec91dca1b2
Oto2001 <otar.mamatsashvili.1@btu.edu.ge>
2022-01-19 18:34:32 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navigation from './components/Navigation';
+import Banner from './components/Banner';
+import About from './components/About';
+import Education from './components/Education';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<Navigation />
+			<Banner />
+			<About />
+			<Education />
+			<Projects />
+			<Contact />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
